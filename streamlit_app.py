9da4d0355485ef7facf2fb4f33ab46aa25baa9af
UnniAmbady <ambady1960@hotmail.com>
2024-12-18 23:37:22 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,16 +7,31 @@ st.write(
     "This is a simple chatbot "
     )
 
+context = """
+        [Building and Construction Authority (BCA), Singapore Building Regulations, and DfMA]
+        [You are an intelligent assistant trained to provide detailed and 
+        accurate information about "Design for Manufacturing and Assembly (DFMA)" principles, 
+        as outlined by the Building and Construction Authority (BCA) of Singapore.]
+        [Here is information directly from the official BCA DFMA website:
+        "https://www1.bca.gov.sg/buildsg/productivity/design-for-manufacturing-and-assembly-dfma]
+        [Based on the above information, answer the following question from the user:]      
+        """
+suffix = "Please provide a concise and clear response not exceeding 100 words."
 # Ask user for their OpenAI API key via `st.text_input`.
+
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+#openai_api_key = st.text_input("OpenAI API Key", type="password")
+
+openai_api_key = st.secrets["openai"]["secret_key"]
+client = OpenAI(api_key=openai_api_key)
+
+if not client:
+    st.info("API Key is missing.", icon="🗝️")
 else:
 
     # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+    #client = OpenAI(api_key=openai_api_key)
 
     # Create a session state variable to store the chat messages. This ensures that the
     # messages persist across reruns.
